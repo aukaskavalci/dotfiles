8e10b1128601502f96475eee51fe0d63b91f8537
Halil Kaskavalci <1646238+kaskavalci@users.noreply.github.com>
2021-02-01 07:56:41 +0100

    add powerline todo

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zs
 
 echo Install zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo TODO: install powerline https://powerline.readthedocs.io/en/master/installation/osx.html
